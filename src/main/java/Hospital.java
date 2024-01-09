274b055ba18c72bc191e84b8012bbc5ba33f0a6e
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-09 23:15:12 +0300

    Hospital-v1

--- src/main/java/Hospital.java
+++ src/main/java/Hospital.java
@@ -1,2 +1,20 @@
-public class Hospital {
+public class Hospital extends DataBase{
+    Doctor doctor= new Doctor();
+    Patient patient = new Patient();
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
 }
