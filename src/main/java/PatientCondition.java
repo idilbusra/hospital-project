d5034ab115abd961c014fe944555b67530b55feb
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-09 23:10:36 +0300

    Patient-v1

--- src/main/java/PatientCondition.java
+++ src/main/java/PatientCondition.java
@@ -1,2 +1,20 @@
 public class PatientCondition {
+    private String actualCondition;
+    private boolean urgency;
+
+    public String getActualCondition() {
+        return actualCondition;
+    }
+
+    public void setActualCondition(String actualCondition) {
+        this.actualCondition = actualCondition;
+    }
+
+    public boolean isUrgency() {
+        return urgency;
+    }
+
+    public void setUrgency(boolean urgency) {
+        this.urgency = urgency;
+    }
 }
